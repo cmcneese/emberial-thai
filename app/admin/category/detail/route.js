c56429a4874b5c184a5451463025cad10e822534
Chris McNeese <christophermcneese@mac.com>
2017-01-19 15:00:35 -0600

    grid item wrapper

--- app/admin/category/detail/route.js
+++ app/admin/category/detail/route.js
@@ -2,29 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return[
-    {
-  name: 'Main Courses',
-  menuItems: [
-    {
-      name: 'Super Salad',
-      price: 6,
-      description: 'Cabbage, Bacon, Egg, Nori Green Goddess'
-    },
-    {
-      name: 'Buttered Noodles',
-      price: 9,
-      description: 'Miso Butter, Pepper, Hand Pulled Noodles'
-    },
-    {
-      name: 'Hot Chicken',
-      price: 12,
-      description: 'Fried Chicken Breast, Nashville Spices'
-    },
-  ]
-}
-
-]
-
+    return {
+      name: 'Main Courses',
+      menuItems: [
+        {
+          name: 'Super Salad',
+          price: 6,
+          description: 'Cabbage, Bacon, Egg, Nori Green Goddess'
+        }, {
+          name: 'Buttered Noodles',
+          price: 9,
+          description: 'Miso Butter, Pepper, Hand Pulled Noodles'
+        }, {
+          name: 'Hot Chicken',
+          price: 12,
+          description: 'Fried Chicken Breast, Nashville Spices'
+        }
+      ]
+    };
   }
 });
