b1a5cd45d7bbcd7d6cbba153c78037b605025458
Chris McNeese <christophermcneese@mac.com>
2017-01-19 14:24:09 -0600

    detail route model

--- app/admin/category/detail/route.js
+++ app/admin/category/detail/route.js
@@ -1,23 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return[
+    {
+  name: 'Main Courses',
+  menuItems: [
+    {
+      name: 'Super Salad',
+      price: 6,
+      description: 'Cabbage, Bacon, Egg, Nori Green Goddess'
+    },
+    {
+      name: 'Buttered Noodles',
+      price: 9,
+      description: 'Miso Butter, Pepper, Hand Pulled Noodles'
+    },
+    {
+      name: 'Hot Chicken',
+      price: 12,
+      description: 'Fried Chicken Breast, Nashville Spices'
+    },
+  ]
+}
 
-    name: 'Main Courses',
-    menuItems: [
-      {
-        name: 'Super Salad',
-        price: 6,
-        description: 'Cabbage, Bacon, Egg, Nori Green Goddess'
-      },
-      {
-        name: 'Buttered Noodles',
-        price: 9,
-        description: 'Miso Butter, Pepper, Hand Pulled Noodles'
-      },
-      {
-        name: 'Hot Chicken',
-        price: 12,
-        description: 'Fried Chicken Breast, Nashville Spices'
-      },
-    ]
-  });
+]
+
+  }
+});
